01b7cc9ef74379d602142ae95e2afb6442738ce4
Malendra <malendra.hurbuns@myob.com>
2016-10-13 16:12:05 +1300

    initial check in

---
+++ src/PuhoiAPI/AutoMapperProfileConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Puhoi.DataModels;
+using Puhoi.Models.Models;
+
+namespace PuhoiAPI
+{
+    public class AutoMapperProfileConfiguration : Profile
+    {
+        protected override void Configure()
+        {
+            CreateMap<StoreDto, StoreModel>().
+                ForSourceMember("StoreId", s => s.Ignore()).
+                ForSourceMember("UId", s => s.Ignore()).
+                ForMember("Id", s => s.Ignore()).
+                AfterMap((dto, model) => model.Id = dto.UId);
+
+            CreateMap<StoreModel, StoreDto>()
+                //ForMember("UId", s => s.Ignore()).
+                .AfterMap((model, dto) => dto.UId = model.Id);
+        }
+
+
+    }
+}
